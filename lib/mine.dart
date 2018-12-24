fcb6ad8c6381ac847a33b99ff59e55894bc7e30c
董建新 <824629419@qq.com>
2018-12-24 11:52:13 +0800

    video add failed

--- lib/mine.dart
+++ lib/mine.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:video_player/video_player.dart';
 
 class Mine extends StatefulWidget {
   @override
@@ -6,21 +7,56 @@ class Mine extends StatefulWidget {
 }
 
 class MineState extends State<Mine> with SingleTickerProviderStateMixin {
+//  VideoPlayerController _controller;
+//  bool _isPlaying = false;
+
+//  @override
+//  void initState() {
+//    super.initState();
+//    _controller = VideoPlayerController.network(
+//        'http://static.smartisanos.cn/common/video/proud-farmer.mp4');
+//
+//    _controller.addListener(() {
+//      final bool isPlaying = _controller.value.isPlaying;
+//      if (isPlaying != _isPlaying) {
+//        setState(() {
+//          _isPlaying = isPlaying;
+//        });
+//      }
+//    });
+//
+//    _controller.initialize().then((_) {
+//      setState(() {});
+//    });
+//  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Video Player',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text("Mine"),
+          title: new Text("Video Player"),
           centerTitle: true, // 是否让title居中
           leading: new Icon(Icons.face),
           automaticallyImplyLeading: true,
         ),
-        body: new Text('Mine'),
+//        body: Center(
+//            child: _controller.value.initialized
+//                ? AspectRatio(
+//                    aspectRatio: _controller.value.aspectRatio,
+//                    child: VideoPlayer(_controller),
+//                  )
+//                : Container()),
+//        floatingActionButton: FloatingActionButton(
+//          onPressed: _controller.value.isPlaying
+//              ? _controller.pause
+//              : _controller.play,
+//          child: Icon(_controller.value.isPlaying?Icons.pause:Icons.play_arrow),
+//        ),
       ),
     );
   }
